61c472c7338765702ffc2ed0e4156f14ab9edc79
“Temitope <temtopeayobami@gmail.com>
2023-07-12 19:00:31 +0100

    update

--- app_folder/views.py
+++ app_folder/views.py
@@ -53,14 +53,14 @@ class DetailProjectView(DetailView):
     model = Post
     template_name = 'apps/details.html'
     
-
+'''
     def get_context_data(self, *args, **kwargs):
         context = super(DetailProjectView, self).get_context_data(**kwargs)
         stuff = get_object_or_404(Post, id= self.kwargs['pk'])
         total_likes = stuff.total_likes()
         context ["total_likes"] = total_likes
         return context
-    
+    '''
     
        
 class AddBlogView(CreateView):
