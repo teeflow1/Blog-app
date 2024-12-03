68ce6b83313c8f48903e57ed1511219278cb9634
-add <temtopeayobami@gmail.com>
2024-12-03 13:04:53 +0100

    Work

--- members/views.py
+++ members/views.py
@@ -14,7 +14,7 @@ class CreateProfilePageView(CreateView):
     template_name = 'registration/create_user_profile.html'
     form_class = ProfilePageForm
 
-fkf
+
     
     def form_valid(self, form):
         form.instance.user = self.request.user
